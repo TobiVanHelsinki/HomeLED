71003b09810ce3229533720b8e46b0cbdcc9f874
Tobi van Helsinki <TobiVanHelsinki@users.noreply.github.com>
2021-11-12 18:56:49 +0100

    added todo

--- HomeLedApp/UI/MainPage.xaml.cs
+++ HomeLedApp/UI/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Xamarin.Forms;
 
 namespace HomeLedApp.UI
 {
+    //TODO +- buttons for sliders and input field for number of leds and the data pin in the settings
     public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
         #region NotifyPropertyChanged
